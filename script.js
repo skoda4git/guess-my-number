1885573bf4d7b0e5e8cc44639f37f8e3369d558c
skoda4git <skodainnit@gmail.com>
2025-08-16 16:19:35 +0400

    added score system

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let minNum = 1;
 let maxNum = 20;
 let num = Math.floor(Math.random() * maxNum) + minNum;
 console.log(num);
+let score = 0;
+let scoreLabel = document.getElementById("scoreLabel")
 mySubmit.onclick = function () {
     let guess = Number(myInput.value);
     if (guess === num) {
@@ -22,11 +24,23 @@ mySubmit.onclick = function () {
     } else if (guess > num) {
         myGuess.textContent = `Your guess was ${guess}.`;
         myAnswer.textContent = "Too high!";
+        score ++;
+        scoreLabel.textContent = `Score: ${score}`;
     } else if (guess < num) {
         myGuess.textContent = `Your guess was ${guess}.`;
         myAnswer.textContent = "Too low";
+        score ++;
+        scoreLabel.textContent = `Score: ${score}`;
     } else{
         myGuess.textContent = "";
         myAnswer.textContent = "error";
+    }
+    if(score === 15){
+        myGuess.textContent = "Your score has reached 15";
+        myAnswer.textContent = `The answer was ${num}. Game has restarted`;
+        score = 0;
+        scoreLabel.textContent = `Score: ${score}`;
+        num = Math.floor(Math.random() * maxNum) + minNum;
+        console.log("Restart number:", num);
     }
 }
